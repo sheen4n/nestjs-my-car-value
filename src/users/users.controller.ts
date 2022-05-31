e83f9b393d49af4a53b8547598a7416f55a91a52
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2022-05-31 15:44:22 +0800

    Remove colors endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Patch,
   Post,
   Query,
-  Session,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
@@ -24,16 +23,6 @@ export class UsersController {
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
-
-  @Get('/colors/:color')
-  setColor(@Param('color') color: string, @Session() session: any) {
-    session.color = color;
-  }
-
-  @Get('/colors')
-  getColor(@Session() session: any) {
-    return session.color;
-  }
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
