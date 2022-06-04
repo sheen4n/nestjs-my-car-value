13066d9dd7a0aa5273d63b2d0fc71fa50b651588
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2022-06-04 23:42:28 +0800

    Added extra test for e2e test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -25,4 +25,21 @@ describe('Authentication System (e2e)', () => {
     expect(response.body.id).toBeDefined();
     expect(response.body.email).toEqual(email);
   });
+
+  it('/auth/signup as a new user then get the currently logged in user via /auth/whoami', async () => {
+    const email = 'blahblah@abc.com';
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'abcd' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    const whoamiResponse = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(whoamiResponse.body.email).toEqual(email);
+  });
 });
