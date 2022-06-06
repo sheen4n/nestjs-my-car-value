d94e1800e8feff54307bb253ba3a4ecc1f62465f
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2022-06-06 16:56:46 +0800

    Make cookie key configurable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
-import { APP_PIPE } from '@nestjs/core';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import {MiddlewareConsumer, Module, ValidationPipe} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {ReportsModule} from './reports/reports.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {User} from './users/user.entity';
+import {Report} from './reports/report.entity';
+import {APP_PIPE} from '@nestjs/core';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: `.env.${ process.env.NODE_ENV }`,
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
@@ -31,11 +31,15 @@ const cookieSession = require('cookie-session');
   controllers: [AppController],
   providers: [
     AppService,
-    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+    {provide: APP_PIPE, useValue: new ValidationPipe({whitelist: true})},
   ],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(cookieSession({ keys: ['adasdad'] })).forRoutes('*');
+
+  constructor (private configService: ConfigService) {}
+
+
+  configure (consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({keys: [this.configService.get('COOKIE_KEY')]})).forRoutes('*');
   }
 }
