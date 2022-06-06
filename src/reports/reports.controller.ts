45490da0f1ff7da884839db3535444507ba881ce
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2022-06-06 16:36:18 +0800

    Setup get-estimate dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Param, Patch, Post, UseGuards} from '@nestjs/common';
+import {Body, Controller, Get, Param, Patch, Post, Query, UseGuards} from '@nestjs/common';
 import {AdminGuard} from 'src/guards/admin.guard';
 import {AuthGuard} from 'src/guards/auth.guard';
 import {Serialize} from 'src/interceptors/serialize.interceptor';
@@ -6,6 +6,7 @@ import {CurrentUser} from 'src/users/decorators/current-user.decorator';
 import {User} from 'src/users/user.entity';
 import {ApproveReportDto} from './dtos/approve-report.dto';
 import {CreateReportDto} from './dtos/create-report.dto';
+import {GetEstimateDto} from './dtos/get-estimate.dto';
 import {ReportDto} from './dtos/report.dto';
 import {Report} from './report.entity';
 import {ReportsService} from './reports.service';
@@ -13,6 +14,9 @@ import {ReportsService} from './reports.service';
 @Controller('reports')
 export class ReportsController {
   constructor (private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate (@Query() query: GetEstimateDto) {}
 
   @Post()
   @UseGuards(AuthGuard)
