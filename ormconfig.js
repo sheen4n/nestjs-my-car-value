bf84979aeafe8f49046f2ac8e0344ef4973c4307
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2022-06-06 18:00:32 +0800

    Add environment config for heroku

--- ormconfig.js
+++ ormconfig.js
@@ -24,6 +24,15 @@ switch (process.env.NODE_ENV) {
     })
     break
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL, // from heroku
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnathorized: false,
+      }
+    })
     break
   default:
     throw new Error("unknown environment")
